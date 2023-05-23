748e131f2e06c4a69e17f1a098e7fe003c50536a
IvanTrukhin <trukhinivan25@gmail.com>
2023-05-23 19:31:31 +0500

    creating new classes

--- UIKitColourApp/ViewController.swift
+++ UIKitColourApp/ViewController.swift
@@ -16,12 +16,9 @@ class ViewController: UIViewController {
     @IBOutlet var labelBlue: UILabel!
     
     
-    @IBOutlet var sliderRed: UISlider!
-    @IBOutlet var sliderGreen: UISlider!
-    @IBOutlet var sliderBlue: UISlider!
-    
-    
-    @IBOutlet var slidersCollection: [UISlider]!
+    @IBOutlet var sliderRed: ConfigureSlider!
+    @IBOutlet var sliderGreen: ConfigureSlider!
+    @IBOutlet var sliderBlue: ConfigureSlider!
     
     
     override func viewDidLoad() {
@@ -34,19 +31,6 @@ class ViewController: UIViewController {
         sliderGreen.minimumTrackTintColor = .green
         sliderBlue.minimumTrackTintColor = .blue
         
-        // MARK: - Slider Scale
-        
-        for slider in slidersCollection {
-            configureSlider(slider)
-        }
-        
-        func configureSlider(_ slider: UISlider) {
-            slider.value = 1
-            slider.minimumValue = 0
-            slider.maximumValue = 255
-            slider.maximumTrackTintColor = .systemBackground
-        }
-        
         // MARK: - label Сhange
         labelRed.text = String("Red: \(sliderRed.value)")
         labelGreen.text = String("Green: \(sliderGreen.value)")
@@ -54,22 +38,21 @@ class ViewController: UIViewController {
     }
     
         // MARK: - label Change With Value Translation
-    
-    func changeColourView() {
+    func changeColorView() {
         viewColour.backgroundColor = UIColor(red: CGFloat(sliderRed.value) / 255, green: CGFloat(sliderGreen.value) / 255, blue: CGFloat(sliderBlue.value) / 255, alpha: 1)
     }
     
     @IBAction func sliderRedAction(_ sender: Any) {
         labelRed.text = String("Red: \(Int(sliderRed.value))")
-        changeColourView()
+        changeColorView()
     }
     @IBAction func sliderGreenAcion(_ sender: Any) {
         labelGreen.text = String("Green: \(Int(sliderGreen.value))")
-        changeColourView()
+        changeColorView()
     }
     @IBAction func sliderBlueAction(_ sender: Any) {
         labelBlue.text = String("Blue: \(Int(sliderBlue.value))")
-        changeColourView()
+        changeColorView()
     }
 }
 
