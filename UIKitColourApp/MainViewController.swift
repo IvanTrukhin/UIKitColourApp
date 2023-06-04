150d4f0065f14770f6fb24863352afe8242d5f4b
IvanTrukhin <trukhinivan25@gmail.com>
2023-06-04 15:53:28 +0500

    view layout

---
+++ UIKitColourApp/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  UIKitColourApp
+//
+//  Created by Ivan on 04.06.2023.
+//
+
+import UIKit
+
+class MainViewController: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
